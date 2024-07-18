85ac85f0b603199eb0406ab460a05db828b59928
JPaonaskar <61259284+JPaonaskar@users.noreply.github.com>
2024-07-18 09:43:43 -0700

    Added search, improved testing to mimic real case, and added simple timing

--- compute_tools.py
+++ compute_tools.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.signal import fftconvolve # potentially faster method with numpy?: https://medium.com/latinxinai/vectorized-convolution-operation-using-numpy-b122fd52fba3
 
-def batch_correlate(kernals:np.ndarray, images:np.ndarray, mode:str='full') -> np.ndarray:
+def batch_correlate(images:np.ndarray, kernals:np.ndarray, mode:str='full') -> np.ndarray:
     '''
     Correlation for batches
 
@@ -15,9 +15,20 @@ def batch_correlate(kernals:np.ndarray, images:np.ndarray, mode:str='full') -> n
     # flip kernal vertically (axis=1) and horizontally (axis=2)
     kernals = np.flip(np.flip(kernals, axis=1), axis=2)
 
-    # normalize
-    images = images.astype(np.float32) # - np.mean(images, axis=[1, 2])
-    kernals = kernals.astype(np.float32) # - np.mean(kernals, axis=[1, 2])
+    # get means
+    image_means = np.mean(images, axis=(1, 2))
+    kernal_means = np.mean(kernals, axis=(1, 2))
+
+    # reshape means
+    image_means = image_means.reshape((images.shape[0], 1, 1))
+    image_means = np.tile(image_means, (1, images.shape[1], images.shape[2]))
+
+    kernal_means = kernal_means.reshape((kernals.shape[0], 1, 1))
+    kernal_means = np.tile(kernal_means, (1, kernals.shape[1], kernals.shape[2]))
+
+    # shift
+    images = images - image_means
+    kernals = kernals - kernal_means
 
     # convolve
     out = fftconvolve(images, kernals.conj(), mode=mode, axes=[1, 2])
@@ -25,7 +36,7 @@ def batch_correlate(kernals:np.ndarray, images:np.ndarray, mode:str='full') -> n
     # output
     return out
 
-def sub_pixel_peak(batch:np.ndarray) -> tuple[float, float]:
+def sub_pixel_peak(batch:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     '''
     Find the location of the peak at the sub pixel level
 
@@ -33,6 +44,22 @@ def sub_pixel_peak(batch:np.ndarray) -> tuple[float, float]:
         batch (np.ndarray) : batch of images
 
     Returns:
-        rows (float) : peak row locations
-        cols (float) : peak column locations
+        rows (np.ndarray) : peak row locations
+        cols (np.ndarray) : peak column locations
     '''
+    # get max values for each image
+    maxs = np.amax(batch, axis=(1, 2))
+
+    # reshape to use in np.where
+    maxs = maxs.reshape((batch.shape[0], 1, 1))
+    maxs = np.tile(maxs, (1, batch.shape[1], batch.shape[2]))
+
+    # get indecies
+    inds = np.where(batch == maxs)
+
+    # unpack values
+    x = inds[1]
+    y = inds[2]
+
+    # output
+    return x, y
--- testing.py
+++ testing.py
@@ -5,21 +5,74 @@ import schlieren
 #bos.compute(start=0, stop=5)
 #bos.display(schlieren.DATA_COMPUTED)
 
+import time
 import numpy as np
 from compute_tools import *
 
-a = np.arange(25).reshape(1, 5, 5)
-a = np.vstack([a, a+1, a+2, a+3, a+4])
+n = 30
+w = 64
+h = 64
+kw = 32
+kh = 32
+
+# cross pattern
+pat = np.array([[0, 1, 0],
+              [1, 1, 1],
+              [0, 1, 0]])
+
+# noise function
+def noise(shape:tuple, scale:float=0.2) -> np.ndarray:
+    # create random numbers
+    out = np.random.random(shape).astype(np.float16)
+
+    # center and scale
+    out = (2 * out - 1) * scale
+
+    # output
+    return out
+
+# create kernal
+k = np.zeros((kh, kw))
+
+# center pattern
+ph, pw = pat.shape
+row = (kh - ph) // 2
+col = (kw - pw) // 2
+k[row:row+ph, col:col+pw] = pat
+print('##### K #####')
+print(k.shape)
+
+# create images
+t0 = time.time()
+a = noise((n, h, w)) # noise
+for i in range(n):
+    a[i, i:i+k.shape[0], i:i+k.shape[1]] += k
+
+print('##### A #####')
 print(a.shape)
 
-b = np.array([[[0, 1], [5, 6]]])
-b = np.vstack([b] * 5)
+# create kernals
+b = k.reshape((1, *k.shape))
+b = np.vstack([b] * n).astype(np.float16)
+b += noise((n, *k.shape)) # noise
+
+print('##### B #####')
 print(b.shape)
 
-c = batch_correlate(a, b, 'valid').astype(np.int32)
-
+# corrilate
+t1 = time.time()
+c = batch_correlate(a, b, 'valid')
+print('##### C #####')
 print(c.shape)
-print(a)
-print(b)
-print('################')
-print(c)
+
+# find
+t2 = time.time()
+x, y = sub_pixel_peak(c)
+print('##### X Y #####')
+print(x, y)
+
+t3 = time.time()
+
+print(f'Creation time: {1000 * (t2 - t1)} ms')
+print(f'Corrilation time: {1000 * (t2 - t1)} ms')
+print(f'Search time: {1000 * (t3 - t2)} ms')
